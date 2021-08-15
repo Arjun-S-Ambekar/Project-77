3c781251944d861232e0e1ef63feb5f6468073f2
Arjun-S-Ambekar <80889604+Arjun-S-Ambekar@users.noreply.github.com>
2021-08-15 12:43:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var first_name="Arjun";
+var middle_name=" Sachin";
+var last_name=" Ambekar";
+var full_name=first_name.concat(middle_name+last_name);
+console.log(full_name);
+var A= 10;
+var b= 5;
+var add= A + b;
+console.log(add);
